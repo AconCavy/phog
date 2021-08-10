ddb933738af8c8a2de14d26c2ded27c87689c0c2
AconCavy <aconcavy@gmail.com>
2021-08-11 05:30:15 +0900

    Initial Commit

---
+++ Phog/PhogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhogApp.swift
+//  Phog
+//
+//  Created by Acon on 2021/08/11.
+//
+
+import SwiftUI
+
+@main
+struct PhogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
